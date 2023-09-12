71582938bcbca87026d9f61472b630972b15b075
redchien <red.chien45697@gmail.com>
2023-09-12 19:56:34 +0800

    add new function of uploadthing

--- components/modals/Initial-modal.tsx
+++ components/modals/Initial-modal.tsx
@@ -25,6 +25,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
+import { FileUpload } from "@/components/ui/file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -76,7 +77,21 @@ export const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Image Upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}
@@ -91,7 +106,7 @@ export const InitialModal = () => {
                         disabled={isLoading}
                         className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
                         placeholder="Enter server name"
-                        // {...field}
+                        {...field}
                       />
                     </FormControl>
                     <FormMessage />
