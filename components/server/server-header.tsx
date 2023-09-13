4b8619c08d84ba8de3ec1679bd2a934e39f368fe
redchien <red.chien45697@gmail.com>
2023-09-13 11:47:37 +0800

    invite action

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -18,6 +18,7 @@ import {
   Trash,
   Users,
 } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
@@ -27,6 +28,8 @@ interface ServerHeaderProps {
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
+
+  const { onOpen } = useModal();
 
   return (
     <DropdownMenu>
@@ -38,7 +41,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
-          <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("invite", { server })}
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+          >
             Invite People
             <Plus className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
