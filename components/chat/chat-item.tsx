b983303aca8a6b9a85ac68408d0621957bd6fbf4
redchien <red.chien45697@gmail.com>
2023-09-14 13:42:44 +0800

    fix the next.config.js to fit the socket.io bug and add the click member to jump into the conversation page

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -17,6 +17,8 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useModal } from "@/hooks/use-modal-store";
 
+import { useRouter, useParams } from "next/navigation";
+
 interface ChatItemProps {
   id: string;
   content: string;
@@ -58,6 +60,8 @@ export const ChatItem = ({
   const [isDeleting, setIsDeleting] = useState(false);
 
   const { onOpen } = useModal();
+  const router = useRouter();
+  const params = useParams();
 
   useEffect(() => {
     const handleKeyDown = (event: any) => {
@@ -95,6 +99,14 @@ export const ChatItem = ({
 
   const isLoading = form.formState.isSubmitting;
 
+  const onMemberClick = () => {
+    if (member.id === currentMember.id) {
+      return;
+    }
+
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  };
+
   const onsubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       const url = qs.stringifyUrl({
@@ -113,13 +125,19 @@ export const ChatItem = ({
   return (
     <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
       <div className="group flex gap-x-2 items-start w-full">
-        <div className="cursor-pointer hover:drop-shadow-md transition">
+        <div
+          onClick={onMemberClick}
+          className="cursor-pointer hover:drop-shadow-md transition"
+        >
           <UserAvatar src={member.profile.imageUrl} />
         </div>
         <div className="flex flex-col w-full">
           <div className="flex items-center gap-x-2">
             <div className="flex items-center">
-              <p className="font-semibold text-sm hover:underline">
+              <p
+                onClick={onMemberClick}
+                className="font-semibold text-sm hover:underline"
+              >
                 {member.profile.name}
               </p>
               <ActionTooltip label={member.role}>
