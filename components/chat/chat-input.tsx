17ecca0a376b3b35a79bff09552894d42af7169b
redchien <red.chien45697@gmail.com>
2023-09-14 09:58:37 +0800

    add file attachment in message

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -10,6 +10,7 @@ import axios from "axios";
 import { Form, FormItem, FormField, FormControl } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Plus, Smile } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -23,6 +24,8 @@ const formSchema = z.object({
 });
 
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const { onOpen } = useModal();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -56,8 +59,8 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
               <FormControl>
                 <div className="relative p-4 pb-6">
                   <button
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
                     type="button"
-                    onClick={() => {}}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]" />
